d51b559387cea92ebcfcd098e825f79168c3dda7
sktomsi <designsbytomc@gmail.com>
2025-02-10 07:00:24 +0000

    add intro for tones, add 3rd set

--- src/app/(info)/resources/page.tsx
+++ src/app/(info)/resources/page.tsx
@@ -6,6 +6,7 @@ import {
 } from "@/components/ui/accordion";
 import { Card } from "@/components/ui/card";
 import { LITURGICAL_TONES } from "@/lib/constants";
+import { Info } from "lucide-react";
 
 export default function Page() {
 	return (
@@ -18,6 +19,23 @@ export default function Page() {
 				<p className="text-xl text-gray-600 dark:text-gray-400 tracking-tight">
 					Curated Resources by Bombay MGOCSM
 				</p>
+			</div>
+			<div className="flex flex-col gap-4  bg-primary-light/20 rounded-xl p-5">
+				<div className="flex items-start gap-3 w-full">
+					<Info className="w-8 h-8 mt-1" />
+					<div className="flex flex-col gap-1 w-full">
+						<h3 className="text-2xl font-bold tracking-tight">
+							Introduction to Liturgical Tones
+						</h3>
+						<p>- by Rev. Fr. Skaria Thomas (Siby Achen)</p>
+						{/* biome-ignore lint/a11y/useMediaCaption: <explanation> */}
+						<audio
+							controls
+							src={"/audios/Intro-audio.mp3"}
+							className="w-full mt-5"
+						/>
+					</div>
+				</div>
 			</div>
 			<div className="flex flex-col gap-4 lg:px-10">
 				<p className="text-2xl tracking-tight font-bold">Liturgical Tones</p>
@@ -51,6 +69,29 @@ export default function Page() {
 						</AccordionTrigger>
 						<AccordionContent className="flex flex-col gap-4 lg:grid lg:grid-cols-4">
 							{LITURGICAL_TONES["category-2"].map((tone) => (
+								<Card key={tone.id} className="p-4">
+									<div className="flex flex-col gap-4">
+										<p className="text-base font-medium tracking-tight">
+											Tone {tone.id}
+										</p>
+										{/* biome-ignore lint/a11y/useMediaCaption: <explanation> */}
+										<audio
+											controls
+											src={tone.audio}
+											className="w-full bg-white"
+											style={{ backgroundColor: "white" }}
+										/>
+									</div>
+								</Card>
+							))}
+						</AccordionContent>
+					</AccordionItem>
+					<AccordionItem value="item-3">
+						<AccordionTrigger>
+							Category 2 - Boovoso of Mar Yakob
+						</AccordionTrigger>
+						<AccordionContent className="flex flex-col gap-4 lg:grid lg:grid-cols-4">
+							{LITURGICAL_TONES["category-3"].map((tone) => (
 								<Card key={tone.id} className="p-4">
 									<div className="flex flex-col gap-4">
 										<p className="text-base font-medium tracking-tight">
--- src/features/landing-page/components/hero-section.tsx
+++ src/features/landing-page/components/hero-section.tsx
@@ -1,145 +1,145 @@
 "use client";
 
 import { IMAGES } from "@/assets";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import React from "react";
 import { CustomButton } from "./CustomButtom";
-import { motion } from "framer-motion";
 // import { FlipWords } from "@/components/ui/flip-words";
 
 // const words = ["मुंबई", "Bombay", "മുംബൈ"];
 
 export function Hero() {
-  return (
-    <div className="flex flex-col items-center justify-items-center gap-4 relative w-full text-center pb-10 md:pb-30 pt-4 md:pt-0">
-      <motion.div className="-z-10 bg-[url('/blue-bg.webp')] bg-no-repeat absolute bg-cover max-sm:bg-center inset-0 top-[50%] md:top-80" />
-      <div className="absolute inset-0 -z-20 bg-[url('/contour.png')] bg-cover bg-center opacity-20 [mask-image:linear-gradient(to_top,transparent,black_20%,black_80%,transparent)]" />
-      <div className="h-full w-full absolute top-0 left-0 bg-brand-bg -z-30" />
+	return (
+		<div className="flex flex-col items-center justify-items-center gap-4 relative w-full text-center pb-10 md:pb-30 pt-4 md:pt-0">
+			<motion.div className="-z-10 bg-[url('/blue-bg.webp')] bg-no-repeat absolute bg-cover max-sm:bg-center inset-0 top-[50%] md:top-80" />
+			<div className="absolute inset-0 -z-20 bg-[url('/contour.png')] bg-cover bg-center opacity-20 [mask-image:linear-gradient(to_top,transparent,black_20%,black_80%,transparent)]" />
+			<div className="h-full w-full absolute top-0 left-0 bg-brand-bg -z-30" />
 
-      <motion.div className="hidden lg:flex animate-fadeIn">
-        <Image
-          src={IMAGES["worship"]}
-          alt="worship"
-          className="h-48 object-contain absolute top-32 right-20 w-fit animate-float"
-        />
-        <Image
-          src={IMAGES["service"]}
-          alt="worship"
-          className="h-48 object-contain absolute top-[30%] w-fit right-[17%] animate-float delay-1000"
-        />
-        <Image
-          src={IMAGES["study"]}
-          alt="worship"
-          className="h-32 object-contain absolute top-32 left-20 w-fit animate-float delay-1000"
-        />
-      </motion.div>
+			<motion.div className="hidden lg:flex animate-fadeIn">
+				<Image
+					src={IMAGES.worship}
+					alt="worship"
+					className="h-48 object-contain absolute top-32 right-20 w-fit animate-float"
+				/>
+				<Image
+					src={IMAGES.service}
+					alt="worship"
+					className="h-48 object-contain absolute top-[30%] w-fit right-[17%] animate-float delay-1000"
+				/>
+				<Image
+					src={IMAGES.study}
+					alt="worship"
+					className="h-32 object-contain absolute top-32 left-20 w-fit animate-float delay-1000"
+				/>
+			</motion.div>
 
-      <div className="relative flex flex-col items-center justify-center w-full md:max-w-7xl mx-auto px-5 md:pt-20 pt-32">
-        <div className="w-full h-full relative z-10 items-center flex justify-center flex-col ">
-          <motion.p
-            className="tracking-tight  border text-navy-blue border-brand-yellow rounded-full px-5 w-fit bg-[#FDF3D4] mb-4 font-medium text-md"
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{
-              delay: 1.4,
-              duration: 0.8,
-              type: "spring",
-              damping: 40,
-              stiffness: 100,
-            }}
-          >
-            MGOCSM - DIOCESE OF BOMBAY
-            <Image
-              src={IMAGES["christmas-theme"].bells}
-              alt="hat"
-              className="md:hidden absolute w-14 h-14 -top-[4.3rem] left-1/2 transform -translate-x-1/2 -rotate-[2deg]"
-            />
-          </motion.p>
-          <motion.h1
-            className="text-[35px] md:text-[56px] font-bold font-crimson tracking-tighter text-brand-yellow text-center leading-relaxed md:leading-normal text-balance w-full"
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{
-              delay: 0.2,
-              duration: 0.8,
-              type: "spring",
-              damping: 40,
-              stiffness: 100,
-            }}
-          >
-            What does it mean to be <br className="md:inline-block hidden" /> an
-            <br className="inline-block md:hidden" />
-            <span className="bg-primary-light px-4 md:px-2 pt-2 text-navy-blue rounded-2xl font-neue md:mx-4 relative">
-              Orthodox Christian <br className="inline-block md:hidden" />
-              <Image
-                src={IMAGES["christmas-theme"].hat}
-                alt="hat"
-                className="hidden md:block absolute md:-top-9 md:-left-5 object-contain  md:w-[60px] md:h-[60px] -rotate-[2deg]"
-              />
-              <Image
-                src={IMAGES["christmas-theme"].bells}
-                alt="hat"
-                className="hidden md:block absolute  md:-bottom-9 md:-right-5 object-contain md:w-[60px] md:h-[60px] -rotate-[2deg]"
-              />
-            </span>
-            youth in <br className="hidden md:block" />
-            <span className="text-navy-blue rounded-2xl font-crimson font-bold mx-4">
-              {/* <FlipWords words={["मुंबई", "Bombay", "മുംബൈ"]} /> */}
-              मुंबई
-            </span>
-            Today?
-          </motion.h1>
-        </div>
-        <motion.div
-          className="relative z-10 flex items-center justify-center px-5 mt-6"
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{
-            delay: 0.6,
-            duration: 0.8,
-            type: "spring",
-            damping: 40,
-            stiffness: 100,
-          }}
-        >
-          <p className="tracking-tighter text-base md:text-lg md:w-[70%] text-center">
-            At Bombay MGOCSM, we explore our faith, embrace our culture, and
-            empower each other to make a difference.
-          </p>
-        </motion.div>
-        <motion.div
-          className="mt-4 px-5"
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{
-            delay: 1.2,
-            duration: 0.8,
-            type: "spring",
-            damping: 40,
-            stiffness: 100,
-          }}
-        >
-          <CustomButton text="Explore our Community" />
-        </motion.div>
-        <motion.div
-          className="my-20 w-full bg-primary-light/20 p-2 rounded-xl md:rounded-3xl border-dashed border-brand-yellow/40 border-2 shadow-2xl"
-          initial={{ opacity: 0, y: 100 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{
-            delay: 2,
-            duration: 0.8,
-            type: "spring",
-            damping: 40,
-            stiffness: 100,
-          }}
-        >
-          <Image
-            src={IMAGES["group-photo"]}
-            alt="group-photo"
-            className="w-full"
-          />
-        </motion.div>
-      </div>
-    </div>
-  );
+			<div className="relative flex flex-col items-center justify-center w-full md:max-w-7xl mx-auto px-5 md:pt-20 pt-32">
+				<div className="w-full h-full relative z-10 items-center flex justify-center flex-col ">
+					<motion.p
+						className="tracking-tight  border text-navy-blue border-brand-yellow rounded-full px-5 w-fit bg-[#FDF3D4] mb-4 font-medium text-md"
+						initial={{ opacity: 0, y: 20 }}
+						animate={{ opacity: 1, y: 0 }}
+						transition={{
+							delay: 1.4,
+							duration: 0.8,
+							type: "spring",
+							damping: 40,
+							stiffness: 100,
+						}}
+					>
+						MGOCSM - DIOCESE OF BOMBAY
+						{/* <Image
+							src={IMAGES["christmas-theme"].bells}
+							alt="hat"
+							className="md:hidden absolute w-14 h-14 -top-[4.3rem] left-1/2 transform -translate-x-1/2 -rotate-[2deg]"
+						/> */}
+					</motion.p>
+					<motion.h1
+						className="text-[35px] md:text-[56px] font-bold font-crimson tracking-tighter text-brand-yellow text-center leading-relaxed md:leading-normal text-balance w-full"
+						initial={{ opacity: 0, y: 20 }}
+						animate={{ opacity: 1, y: 0 }}
+						transition={{
+							delay: 0.2,
+							duration: 0.8,
+							type: "spring",
+							damping: 40,
+							stiffness: 100,
+						}}
+					>
+						What does it mean to be <br className="md:inline-block hidden" /> an
+						<br className="inline-block md:hidden" />
+						<span className="bg-primary-light px-4 md:px-2 pt-2 text-navy-blue rounded-2xl font-neue md:mx-4 relative">
+							Orthodox Christian <br className="inline-block md:hidden" />
+							{/* <Image
+								src={IMAGES["christmas-theme"].hat}
+								alt="hat"
+								className="hidden md:block absolute md:-top-9 md:-left-5 object-contain  md:w-[60px] md:h-[60px] -rotate-[2deg]"
+							/>
+							<Image
+								src={IMAGES["christmas-theme"].bells}
+								alt="hat"
+								className="hidden md:block absolute  md:-bottom-9 md:-right-5 object-contain md:w-[60px] md:h-[60px] -rotate-[2deg]"
+							/> */}
+						</span>
+						youth in <br className="hidden md:block" />
+						<span className="text-navy-blue rounded-2xl font-crimson font-bold mx-4">
+							{/* <FlipWords words={["मुंबई", "Bombay", "മുംബൈ"]} /> */}
+							मुंबई
+						</span>
+						Today?
+					</motion.h1>
+				</div>
+				<motion.div
+					className="relative z-10 flex items-center justify-center px-5 mt-6"
+					initial={{ opacity: 0, y: 20 }}
+					animate={{ opacity: 1, y: 0 }}
+					transition={{
+						delay: 0.6,
+						duration: 0.8,
+						type: "spring",
+						damping: 40,
+						stiffness: 100,
+					}}
+				>
+					<p className="tracking-tighter text-base md:text-lg md:w-[70%] text-center">
+						At Bombay MGOCSM, we explore our faith, embrace our culture, and
+						empower each other to make a difference.
+					</p>
+				</motion.div>
+				<motion.div
+					className="mt-4 px-5"
+					initial={{ opacity: 0, y: 20 }}
+					animate={{ opacity: 1, y: 0 }}
+					transition={{
+						delay: 1.2,
+						duration: 0.8,
+						type: "spring",
+						damping: 40,
+						stiffness: 100,
+					}}
+				>
+					<CustomButton text="Explore our Community" />
+				</motion.div>
+				<motion.div
+					className="my-20 w-full bg-primary-light/20 p-2 rounded-xl md:rounded-3xl border-dashed border-brand-yellow/40 border-2 shadow-2xl"
+					initial={{ opacity: 0, y: 100 }}
+					animate={{ opacity: 1, y: 0 }}
+					transition={{
+						delay: 2,
+						duration: 0.8,
+						type: "spring",
+						damping: 40,
+						stiffness: 100,
+					}}
+				>
+					<Image
+						src={IMAGES["group-photo"]}
+						alt="group-photo"
+						className="w-full"
+					/>
+				</motion.div>
+			</div>
+		</div>
+	);
 }
